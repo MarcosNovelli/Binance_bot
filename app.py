c8a5e3d2aefed2830a0030133171fb80f3a72d9c
Marcos Novelli <marcossnovelli@gmail.com>
2022-12-30 00:00:58 -0300

    basiscs ready

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
-import binance
+import binance 
+from config import CLIENT
+from time import sleep
 
-paper_money = 1
+
+def check_DBD(self):
+
+    first_low = min(list(self.data.low.get(ago=-120, size=60)))
+    second_low = min(list(self.data.low.get(ago=-60, size=60)))
+    third_low = min(list(self.data.low.get(ago=-1, size=60)))
+
+    # Detect DBD formation
+    Dropbd = self.data.open[self.first_candle_open] > self.data.close[self.first_candle_close] and (self.data.close[self.first_candle_close] - first_low) < (self.data.open[self.first_candle_open] - self.data.close[self.first_candle_close])
+    dBased = self.data.close[self.base_close] > self.data.open[self.base_open]
+    dbDrop = self.data.open[self.third_candle_open] > self.data.close[self.third_candle_close] and self.data.close[self.third_candle_close] < second_low and (self.data.open[self.third_candle_open] - self.data.close[self.third_candle_close]) >= ((self.data.close[self.third_candle_close] - self.data.open[self.base_open]) * 2) and self.data.close[self.third_candle_close] < first_low and (self.data.close[self.third_candle_close] - third_low) < (self.data.open[self.third_candle_open] - self.data.close[self.third_candle_close])
+    
+    DBD = Dropbd and dBased and dbDrop
+    
+    self.trade_type = "DBD" if DBD else self.trade_type
+    
+    return DBD
+
+
+
+while True:
+
+    # Need to request X candles from the past to binance
+    # Check if any conditions are met and trade
+    
+
+    
+    sleep(60)
--- config.py
+++ config.py
@@ -1,7 +1,11 @@
-from dotenv import load_dotenv
 import os
-from app import paper_money
+from binance.client import Client
+from dotenv import load_dotenv
+
+paper_money = 1
 
 load_dotenv()
 API_KEY = os.environ["API_KEY_PAPER"] if paper_money else os.environ["API_KEY"]
 API_SECRET = os.environ["API_SECRET_PAPER"] if paper_money else os.environ["API_SECRET"]
+
+CLIENT = Client(API_KEY, API_SECRET)
